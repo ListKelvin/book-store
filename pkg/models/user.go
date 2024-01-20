dd4b3d69201a38066bcf713a7efa8a20da8d344c
Phạm Công Minh <phamm5687@gmail.com>
2024-01-20 23:18:39 +0700

    FIX user model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -4,43 +4,63 @@ import (
 	"time"
 
 	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	ID               uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Name             string    `gorm:"type:varchar(255);not null"`
-	Email            string    `gorm:"uniqueIndex;not null"`
-	Password         string    `gorm:"not null"`
-	Role             string    `gorm:"type:varchar(255);not null"`
-	Provider         string    `gorm:"not null"`
-	Photo            string    `gorm:"not null"`
-	VerificationCode string
-	Verified         bool      `gorm:"not null"`
-	CreatedAt        time.Time `gorm:"not null"`
-	UpdatedAt        time.Time `gorm:"not null"`
+	ID       uuid.UUID `gorm:"type:uuid;primary_key;"`
+	Name     string    `gorm:"type:varchar(100);not null"`
+	Email    string    `gorm:"type:varchar(100);uniqueIndex;not null"`
+	Password string    `gorm:"not null"`
+
+	Role  string `gorm:"type:varchar(20);default:'user';"`
+	Photo string `gorm:"default:'default.png';"`
+
+	Verified  bool      `gorm:"default:false;"`
+	Provider  string    `gorm:"default:'local';"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
 }
 
-type SignUpInput struct {
-	Name            string `json:"name" binding:"required"`
-	Email           string `json:"email" binding:"required"`
-	Password        string `json:"password" binding:"required,min=8"`
-	PasswordConfirm string `json:"passwordConfirm" binding:"required"`
-	Photo           string `json:"photo" binding:"required"`
+func (user *User) BeforeCreate(*gorm.DB) error {
+	user.ID = uuid.NewV4()
+
+	return nil
 }
 
-type SignInInput struct {
-	Email    string `json:"email"  binding:"required"`
-	Password string `json:"password"  binding:"required"`
+type RegisterUserInput struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" bindinig:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginUserInput struct {
+	Email    string `json:"email" bindinig:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 type UserResponse struct {
-	ID        uuid.UUID `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Role      string    `json:"role,omitempty"`
-	Photo     string    `json:"photo,omitempty"`
-	Provider  string    `json:"provider"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string `json:"id,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Role      string `json:"role,omitempty"`
+	Provider  string `json:"provider,omitempty"`
+	Photo     string `json:"photo,omitempty"`
+	Verified  bool   `json:"verified,omitempty"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
+func FilteredResponse(user *User) UserResponse {
+	return UserResponse{
+		ID:        user.ID.String(),
+		Email:     user.Email,
+		Name:      user.Name,
+		Role:      user.Role,
+		Verified:  user.Verified,
+		Photo:     user.Photo,
+		Provider:  user.Provider,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
