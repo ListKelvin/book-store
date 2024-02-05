815786a01a4be7488abd92ddf2f3fa34c3679ea3
Phạm Công Minh <phamm5687@gmail.com>
2024-02-05 15:11:25 +0700

    chore: hide .env

--- api/controllers/app.go
+++ api/controllers/app.go
@@ -21,17 +21,6 @@ type Server struct {
 }
 
 
-func Connect() {
-	dsn := "root:sa123456@tcp(127.0.0.1:3306)/bookStore?charset=utf8mb4&parseTime=True&loc=Local"
-	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-
-	// db.AutoMigrate(&models.User{})
-	fmt.Println("Listening to port", "8080")
-	db = d
-}
 
 func GetDB() *gorm.DB {
 	return db
